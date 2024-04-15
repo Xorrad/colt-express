2223ded6bffe9a10630e0fd9f93596a95b1c0eea
Tristan HB <tristanhb@MacBook-Pro-de-Tristan.local>
2024-04-15 21:38:55 +0200

    j'ai ajouté de quoi faire des tests basiques sur les Toigon et le train

--- src/Modele.java
+++ src/Modele.java
@@ -1,17 +1,22 @@
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 
 public class Modele {//la classe qui contient absolument tout le code
     int tailleTrain = 3; //sert de variable globale pour le nombre de wagon dans un train
     public Toigon[] train; //array (je crois) qui contient les toigons qui constituent le train, ya pas encore de locomotive
     Modele(){
-        this.train = new Toigon[tailleTrain*2]; //La manière dont je visualise le train, c'est que dans un train de taille i, on met les toigons du bas aux indices 0 à i-1, et les toigons du bas aux indices i à 2*i-1, du coup pour passer au toigon du dessus/dessous on ajoute/retire i à l'indice du toigon
+        this.train = new Toigon[tailleTrain*2];
+        this.initTrain();//La manière dont je visualise le train, c'est que dans un train de taille i, on met les toigons du bas aux indices 0 à i-1, et les toigons du bas aux indices i à 2*i-1, du coup pour passer au toigon du dessus/dessous on ajoute/retire i à l'indice du toigon
     }
     public void initTrain(){
-        for (int i=0; i<tailleTrain; i++){
-            this.train[i] = new Toigon();
+        for (int i=0; i<tailleTrain*2; i++){
+            this.train[i] = new Toigon(i);
         }
 
     }
@@ -20,8 +25,23 @@ public class Modele {//la classe qui contient absolument tout le code
     public class Toigon{
         public boolean estToit; //true si le Toigon est le toit, false sinon
         public int numWag; //numéro du wagon, j'imagine qu'on s'en servira pour trouver le suivant/précédent dans la liste?
-        public List<Entity> contenu = new ArrayList<Entity>(); //contient tout ce qui est dans le wagon, peut être ce serait plus simple si on sépare les trésors et les personnages en vrai
+        //Je pense que les deux attributs ci-dessus ne sont pas strictement nécessaires, mais je pense qu'ils simplifieront le code donc pour l'instant je les laisse
+        public List<Entity> contenu; //contient tout ce qui est dans le wagon, peut être ce serait plus simple si on sépare les trésors et les personnages en vrai
+        Toigon(boolean t, int n){
+            this.estToit = t;
+            this.numWag = n;
+            this.contenu  = new ArrayList<Entity>();
 
+        }
+        Toigon( int i){//surcharge de la fonction précédente, cette version sera utilisée pour l'initialisation du train
+            this.estToit = (i>=tailleTrain);
+            this.numWag = i;
+            this.contenu  = new ArrayList<Entity>();
+
+        }
+        boolean getToit(){return this.estToit;}
+        int getNum(){return this.numWag;}
+        List<Entity> getContenu(){return this.contenu;}
     }
 
 
@@ -31,7 +51,7 @@ public class Modele {//la classe qui contient absolument tout le code
     }
 
 
-    public abstract class Trésor extends Entity {
+    public abstract class Tresor extends Entity {
         public int valeur; //should be private probably but it's convenient for now
 
     }
@@ -54,6 +74,15 @@ public class Modele {//la classe qui contient absolument tout le code
         //tests for entity
          Modele m = new Modele();
          System.out.println(m.train.length);
+         for (int i = 0; i<m.train.length; i++){
+
+             if (i< m.tailleTrain) {
+                 assertFalse(m.train[i].getToit());
+             }
+             else{
+                 assertTrue(m.train[i].getToit());
+             }
+         }
     }
 
 }
