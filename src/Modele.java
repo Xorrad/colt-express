d914ad1f1cc1f8e3b415fcf0aebda39b4b308568
Tristan HB <tristanhb@MacBook-Pro-de-Tristan.local>
2024-04-15 23:43:46 +0200

    ajout de méthodes dans toigon pour ajouter/supprimer des Entity, mise en place d'un système de variables globales. faut que je teste plus mais je pense pas que ça va tout casser, sauf si on fait un passage par référence, vu que pour l'instant toutes les entités font référence à une seule entité originale, mais ça pourrait être bien pour quand on fera les bandits pour les localiser

--- src/Modele.java
+++ src/Modele.java
@@ -3,14 +3,23 @@ import java.util.List;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 
 public class Modele {//la classe qui contient absolument tout le code
     int tailleTrain = 3; //sert de variable globale pour le nombre de wagon dans un train
     public Toigon[] train; //array (je crois) qui contient les toigons qui constituent le train, ya pas encore de locomotive
+
+    //variables globales qui sont les éléments de Entity. Ils servent de référence pour les fonctions add et remove
+    Floppa Flop;
+    //fin variables globales
     Modele(){
+
+        //On initalise ici les "variables globales" entités
+        this.Flop = new Floppa ();
+        //fin variables globales
+
+
         this.train = new Toigon[tailleTrain*2];
         this.initTrain();//La manière dont je visualise le train, c'est que dans un train de taille i, on met les toigons du bas aux indices 0 à i-1, et les toigons du bas aux indices i à 2*i-1, du coup pour passer au toigon du dessus/dessous on ajoute/retire i à l'indice du toigon
     }
@@ -27,7 +36,7 @@ public class Modele {//la classe qui contient absolument tout le code
         public int numWag; //numéro du wagon, j'imagine qu'on s'en servira pour trouver le suivant/précédent dans la liste?
         //Je pense que les deux attributs ci-dessus ne sont pas strictement nécessaires, mais je pense qu'ils simplifieront le code donc pour l'instant je les laisse
         public List<Entity> contenu; //contient tout ce qui est dans le wagon, peut être ce serait plus simple si on sépare les trésors et les personnages en vrai
-        Toigon(boolean t, int n){
+        Toigon(boolean t, int n){// normalement, sauf pour éventuellement des tests on devrait pas avoir besoin de cette fonction
             this.estToit = t;
             this.numWag = n;
             this.contenu  = new ArrayList<Entity>();
@@ -40,8 +49,14 @@ public class Modele {//la classe qui contient absolument tout le code
 
         }
         boolean getToit(){return this.estToit;}
+
         int getNum(){return this.numWag;}
+
         List<Entity> getContenu(){return this.contenu;}
+
+        void addEnt(Entity e){this.contenu.add(e);}//marche pour les entités dont on a déclaré une "variable globale" de référence dans modèle
+        void delEnt(Entity e){this.contenu.remove(e);}// retire la première occurence de l'entité e. Pousse à gauche tout élément après e (donc ya pas de trou dans l'array
+        void delEnt(int i){this.contenu.remove(i);}//surcharge de la fonction précédente. retire l'entité d'index i
     }
 
 
@@ -52,7 +67,15 @@ public class Modele {//la classe qui contient absolument tout le code
 
 
     public abstract class Tresor extends Entity {
-        public int valeur; //should be private probably but it's convenient for now
+        private int valeur;
+        abstract int getVal();
+
+    }
+
+
+    public class Floppa extends Tresor {//j'ai créé ça juste pour des tests
+        private int valeur = 99999999;
+        public int getVal(){return this.valeur;}
 
     }
 
@@ -62,7 +85,7 @@ public class Modele {//la classe qui contient absolument tout le code
     }
 
     public class bandit extends Personnage {
-        public int score;//should be private probably but it's convenient for
+        public int score;
 
         public bandit() {
             score = 0;
@@ -73,16 +96,25 @@ public class Modele {//la classe qui contient absolument tout le code
     public static void main(String[] args) {
         //tests for entity
          Modele m = new Modele();
-         System.out.println(m.train.length);
-         for (int i = 0; i<m.train.length; i++){
+         assertEquals(m.tailleTrain*2, m.train.length);//teste que la taille du train (chaque Toigons compte pour 1, donc un wagon et son toit compte pour 2) est bien égal à 2 fois tailleTrain
+         for (int i = 0; i<m.train.length; i++){//teste que l'initialisation du train marche bien
 
              if (i< m.tailleTrain) {
                  assertFalse(m.train[i].getToit());
+                 assertEquals(i,m.train[i].getNum());
              }
              else{
                  assertTrue(m.train[i].getToit());
+                 assertEquals(i,m.train[i].getNum());
              }
          }
+         //juste des tests pour que je vérifie comment marche les arrayList, je suprimerai plus tard
+         m.train[0].addEnt(m.Flop);
+        m.train[0].addEnt(m.Flop);
+         System.out.println(m.train[0].getContenu());
+         m.train[0].delEnt(1);
+        System.out.println(m.train[0].getContenu());
+
     }
 
 }
