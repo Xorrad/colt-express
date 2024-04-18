78b41e69a4492a2235cb83ad3cbb292f2513fdba
Xorrad <monsieurs.aymeric@gmail.com>
2024-04-18 13:04:21 +0200

    test: ajoute tests toigon et entités

--- src/test/Tests.java
+++ src/test/Tests.java
@@ -3,6 +3,9 @@ package test;
 import main.modeles.Direction;
 import main.modeles.Modele;
 import main.modeles.Toigon;
+import main.modeles.entites.Entite;
+import main.modeles.entites.Sheriff;
+import main.modeles.entites.Tresor;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -44,4 +47,68 @@ public class Tests {
         }
     }
 
+    @Test
+    public void testToigon() {
+        Modele m = new Modele();
+        Toigon toigon = m.getToigon(2);
+        Tresor tresor = new Tresor(0, toigon, "Bijou", 500);
+
+        assertFalse(toigon.contientEntite(tresor));
+        toigon.ajouteEntite(tresor);
+        assertTrue(toigon.contientEntite(tresor));
+        toigon.retireEntite(tresor);
+        assertFalse(toigon.contientEntite(tresor));
+
+        toigon.retireEntites(Entite.Type.TRESOR);
+        assertEquals(toigon.getEntites(Entite.Type.TRESOR).size(), 0);
+        toigon.ajouteEntite(tresor);
+        assertEquals(toigon.getEntites(Entite.Type.TRESOR).size(), 1);
+        assertEquals(toigon.getRandomEntite(Entite.Type.TRESOR), tresor);
+
+        // Les numéros, toits et voisins des toigons sont testés dans testTrain().
+    }
+
+    @Test
+    public void testEntite() {
+        Modele m = new Modele();
+        Toigon toigon = m.getToigon(2);
+        Sheriff sheriff = new Sheriff(17, toigon, "Shériff");
+        toigon.ajouteEntite(sheriff);
+
+        // Teste attributs du sheriff.
+        assertEquals(sheriff.getNum(), 17);
+        assertEquals(sheriff.getNom(), "Shériff");
+        assertEquals(sheriff.getType(), Entite.Type.SHERIFF);
+
+        // Teste déplacements depuis wagon milieu
+        assertEquals(sheriff.getToigon(), toigon);
+        assertTrue(sheriff.peutDeplacer(Direction.AVANT));
+        assertTrue(sheriff.peutDeplacer(Direction.ARRIERE));
+        assertTrue(sheriff.peutDeplacer(Direction.HAUT));
+        assertFalse(sheriff.peutDeplacer(Direction.BAS));
+
+        // Teste déplacements depuis toit milieu
+        sheriff.deplace(Direction.HAUT);
+        assertTrue(sheriff.peutDeplacer(Direction.BAS));
+        assertFalse(sheriff.peutDeplacer(Direction.HAUT));
+
+        // Teste si l'entité est retirée/ajoutée aux listes des toigons.
+        assertEquals(sheriff.getToigon(), m.getToigon(3));
+        assertFalse(toigon.contientEntite(sheriff));
+        assertTrue(m.getToigon(3).contientEntite(sheriff));
+
+        // Teste déplacement dernier wagon.
+        sheriff.deplace(Direction.ARRIERE);
+        assertFalse(sheriff.peutDeplacer(Direction.ARRIERE));
+
+        // Teste déplacement locomotive.
+        sheriff.getToigon().retireEntite(sheriff);
+        sheriff.setToigon(m.getToigon(m.getLocomotive().getNumero()-1));
+        sheriff.getToigon().ajouteEntite(sheriff);
+        assertFalse(sheriff.peutDeplacer(Direction.AVANT));
+        sheriff.deplace(Direction.BAS);
+        assertTrue(sheriff.peutDeplacer(Direction.AVANT));
+        sheriff.deplace(Direction.AVANT);
+        assertFalse(sheriff.peutDeplacer(Direction.AVANT));
+    }
 }
