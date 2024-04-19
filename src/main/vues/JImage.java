6a6da6b8545336b01e6c5024a2300ab9ef9c7970
Xorrad <monsieurs.aymeric@gmail.com>
2024-04-19 12:35:31 +0200

    feat: ajoute JImage

---
+++ src/main/vues/JImage.java
@@ -0,0 +1,22 @@
+package main.vues;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class JImage extends JComponent {
+    private Image image;
+
+    public JImage(Image image) {
+        this.image = image;
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(image, 0, 0, this);
+    }
+}
